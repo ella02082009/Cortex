31f4781e62792c4d0a31ff397e1f6dc2e2728fd4
Emmanuella Orjih <emmanuellaorjih08@gmail.com>
2025-09-03 17:31:37 +0100

    Add initial HTML, CSS, and JavaScript files for the Cortex project

---
+++ sign-in.js
@@ -0,0 +1,20 @@
+document.addEventListener('DOMContentLoaded', function(){
+    const signInPage = document.getElementById('signInPage');
+
+    signInPage.addEventListener('submit', function(event){
+        event.preventDefault();
+
+        const emailInput = signInPage.querySelector('input[type="text"]');
+        const passwordInput = signInPage.querySelector('input[type="password"]');
+
+        const emailValue = emailInput.value.trim();
+        const passwordValue = passwordInput.value.trim();
+
+        if(emailValue !== '' && passwordValue !== ''){
+            windows.location.href = 'homepaeg.html';
+        }else{
+            alert('Please fill out both the email and password fields.');
+        }
+
+    });
+});
